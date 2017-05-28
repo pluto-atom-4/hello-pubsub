d100749ce9aabed85c55f82e0aeacb4f8370f820
pluto-atom-4 <genko2@hotmail.com>
2017-05-28 09:41:41 -0700

    INIT

---
+++ src/main/java/internal/example/glcoud/pubsub/snippet/MessageReceiverSnippets.java
@@ -0,0 +1,33 @@
+package internal.example.glcoud.pubsub.snippet;
+
+import com.google.cloud.pubsub.spi.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.spi.v1.MessageReceiver;
+import com.google.pubsub.v1.PubsubMessage;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Methods for receiving pubsub messages
+ */
+public class MessageReceiverSnippets {
+    private final BlockingQueue<PubsubMessage> blockingQueue;
+
+    public MessageReceiverSnippets(BlockingQueue<PubsubMessage> blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    public MessageReceiver messageReceiver() {
+        MessageReceiver receiver;
+        receiver = new MessageReceiver() {
+            @Override
+            public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
+                if (blockingQueue.offer(message)) {
+                    consumer.ack();
+                } else {
+                    consumer.nack();
+                }
+            }
+        };
+        return receiver;
+    }
+}
